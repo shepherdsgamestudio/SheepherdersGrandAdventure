169698217fef19e8b74e3bcca2f97394b7ab520f
Ethan Shafer <ethan.h.shafer@gmail.com>
2019-07-22 19:13:50 -0400

    Added initial states and game logic to proceed with states.

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -1,22 +1,70 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour
 {
-    [SerializeField]
-    Text textComponent;
+    [SerializeField] Text storyText;
+    [SerializeField] Text optionText;
+    [SerializeField] State startingState;
+
+    State currentState;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "Hello, motha fucka.";
+        currentState = startingState;
+        storyText.text = currentState.GetStateStory();
+        optionText.text = currentState.GetStateOptions();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        storyText.text = currentState.GetStateStory();
+        optionText.text = currentState.GetStateOptions();
+    }
+
+    private void ManageState()
+    {
+        State[] nextStates = currentState.GetNextStates();
+        if (nextStates.Length >= 1 && Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            currentState = nextStates[0];
+            return;
+        }
+        else if (nextStates.Length >= 2 && Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            currentState = nextStates[1];
+            return;
+        }
+        else if (nextStates.Length >= 3 && Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            currentState = nextStates[2];
+            return;
+        }
+        if (nextStates.Length >= 1 && Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            currentState = nextStates[0];
+            return;
+        }
+        else if (nextStates.Length >= 2 && Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            currentState = nextStates[1];
+            return;
+        }
+        else if (nextStates.Length >= 4 && Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            currentState = nextStates[3];
+            return;
+        }
     }
 }
---
+++ Assets/State.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)][SerializeField] private string storyText;
+    [SerializeField] private string[] availableOptions;
+    [SerializeField] private State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return this.storyText;
+    }
+
+    public string GetStateOptions()
+    {
+        return string.Join<string>("\n", this.availableOptions);
+    }
+
+    public State[] GetNextStates()
+    {
+        return this.nextStates;
+    }
+}
